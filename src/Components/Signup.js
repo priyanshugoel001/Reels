c58cbc56689b3be55eb28c3ff6ab01d49558d88d
priyanshu goel <priyanshugoel001@gmail.com>
2021-07-11 08:11:59 +0530

    file added

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect,useContext} from 'react'
 import {AuthContext} from '../Context/AuthProvider';
-import { storage,database } from '../Components/firebase';
+import { storage,database } from './firebase';
 function Signup() {
     const [email,setEmail] =useState('');
     const[password,setPassword] = useState('');
@@ -10,81 +10,87 @@ function Signup() {
     const [file,setFile] = useState(null)
     const {signup} =useContext(AuthContext);
     const handleSignup =async (e)=>{
-        try{
         e.preventDefault();
+        try{
         setLoading(true);
         let res = await signup(email,password);
         let uid = res.user.uid;
         console.log(uid); 
-        setLoading(false)
-        const uploadTaskListener=storage.ref(`/users/${uid}/profileImg`).put(file);
-        // fn1  this is for progress
-        // fn2 this is for error
-        // fn3 is for success
-            function fn1(snapshot){
-             var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-             console.log('Upload is ' + progress + '% done');         
-            }
-           function fn2(error){
-             setError(error);
-             setTimeout(()=>{
-                setError('')
-              },2000);
-             setLoading(false)
-            }
-            async function fn3(){
-              let downloadUrl = await uploadTaskListener.snapshot.ref.getDownloadURL();
-        uploadTaskListener.on('state_changed',fn1,fn2,fn3)
-        await database.users.doc(uid).set({
-            email:email,
-            username:name,
-            userId:uid,
-            profileUrl:downloadUrl,
-            createdAt:database.getCurrentTimeStamp(),
-            postId:[]
-        })
-        setLoading(false);
-        console.log("user has signed Up");
-        }}
-        catch(error)
-        {
-            console.log(error);
+        const uploadTaskListener = storage.ref(`/users/${uid}/profileImage`).put(file);
+        // Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+        // fn 1 -> progress tracking
+        // fn2 -> error
+        // fn3 -> success
+        uploadTaskListener.on('state_changed',fn1,fn2,fn3);
+        function fn1(snapshot){
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');         
+        }
+        function fn2(error){
             setError(error);
             setTimeout(()=>{
+                setError('')
             },2000);
-            setLoading(false);
+            setLoading(false)
         }
+        async function fn3(){
+            let downloadUrl = await uploadTaskListener.snapshot.ref.getDownloadURL();
+            console.log(downloadUrl);
+          await database.users.doc(uid).set({
+                email:email,
+                userId:uid,
+                username:name,
+                createdAt:database.getCurrentTimeStamp(),
+                profileUrl:downloadUrl,
+                postIds:[]
+            })
+            setLoading(false);
+            console.log('User has Signed up');
+        }
+    
+      
     }
-    const handlefileSubmit=(e)=>{
-        const file=e.target.files[0];
+    catch(err){
+        setError(err)
+        setTimeout(()=>setError(''),2000);
+        setLoading(false)
+    }
+    }
+    const handleFileSubmit=(e)=>{
+        let file = e.target.files[0];
+        console.log(file);
         if(file!=null)
         {
-            setFile(file);
+            setFile(file)
         }
-       
-    }  
+    }
     return (
         <div>
             <form onSubmit={handleSignup} >
                 <div>
                     <label htmlFor=''>UserName</label>
                     <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
+
                 </div>
                 <div>
-                    <label htmlFor=''>Email</label>
+                <label htmlFor=''>Email</label>
                     <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div>
-                    <label htmlFor=''>Password</label>
+                <label htmlFor=''>Password</label>
                     <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <label htmlFor='profile'>Profile image</label>
-                    <input type='file' accept='image/*' onChange={handlefileSubmit}></input>
+                    <input type='file' accept='image/*' onChange={handleFileSubmit}></input>
                 </div>
                 <button type='submit' disabled={loading}>SignUp</button>
             </form>
         </div>
     )
 }
+
 export default Signup
